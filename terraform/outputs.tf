f0a4702e75855b86c0239fa9c77f3262f91ed47e
Mark P. Lindhout <moin@marklindhout.com>
2022-11-28 10:37:07 +0100

    - Dynamic URL generation for Gitlab Environments.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
 output "server_ip" {
   value = hcloud_server.main.ipv4_address
 }
+
+output "server_https_url_primary" {
+  value = aws_route53_record.primary.name
+}
+
+output "server_https_url_portal" {
+  value = aws_route53_record.portal.name
+}
+
+output "server_https_url_ucs-sso" {
+  value = aws_route53_record.ucs-sso.name
+}
