c8e885cb3f6ec1188013ec5329d321cd78f7af14
Jaime Conde Segovia <conde-segovia.extern@univention.de>
2022-12-22 08:44:53 +0100

    fix(handler): imports missing

--- handler/modules/actions.py
+++ handler/modules/actions.py
@@ -2,7 +2,9 @@ import smtplib
 import ssl
 import requests
 import os
-import datetime
+import json
+from datetime import datetime, timedelta
+import logging
 
 import urllib3
 
@@ -22,8 +24,8 @@ class ActionDelegate:
         self.logger = logging.getLogger(__name__)
 
         self.ucs_udm_rest = os.environ.get("UDM_REST_BASE_URL")
-        self.udm_rest_user =  os.environ.get("UDM_REST_USER")
-        self.udm_rest_password =  os.environ.get("UDM_REST_PASSWORD")
+        self.udm_rest_user = os.environ.get("UDM_REST_USER")
+        self.udm_rest_password = os.environ.get("UDM_REST_PASSWORD")
 
         self.kc_proxy = os.environ.get("KC_PROXY")
 
@@ -33,18 +35,20 @@ class ActionDelegate:
         self.admin_mail = os.environ.get("ADMIN_MAIL")
 
         self.state = state
-        self.proxy = f"http://{self.kc_proxy}/flask-redirect-api"
+        self.proxy = f"http://{self.kc_proxy}/proxy-api"
         self.udm_rest_base_url = "https://{user}:{password}@{uri}".format(
             user=self.udm_rest_user, password=self.udm_rest_password, uri=self.ucs_udm_rest)
         self.fails = fails
         self.condition = condition
 
-        if(self.condition == "ip_address"):
+        if (self.condition == "ipAddress"):
             self.condition_value = "ip"
-            self.proxy_condition = state.get("ip_address")
-        elif(self.condition == "code_id"):
+            self.proxy_condition = state.get("ipAddress")
+        elif (self.condition == "code_id"):
             self.condition_value = "cookie_contains"
             self.proxy_condition = state.get("details").get("code_id")
+        else:
+            print(self.condition)
 
     def trigger(self):
         raise NotImplementedError()
@@ -58,27 +62,27 @@ class ActionBlockIpProxy(ActionDelegate):
     def trigger(self):
         self.logger.debug("IP Proxy")
         self.logger.debug(self.proxy)
-        if(self.fails > 5 and self.condition == "code_id"):
-            payload = {"action": "block", "condition": self.conditionValue,
-                       "value": self.proxyCondition}
+        if (self.fails > 5 and self.condition == "code_id"):
+            payload = {"action": "block", "condition": self.condition_value,
+                       "value": self.proxy_condition, "valid_until": datetime.timestamp(datetime.now() + timedelta(minutes=5))}
             r = requests.post(self.proxy, headers={"Content-Type": "application/json"},
                               json=payload)
-        if(self.fails > 10):
+        if (self.fails > 10):
             payload = {"action": "block", "condition": self.condition_value,
-                       "value": self.proxy_condition}
+                       "value": self.proxy_condition, "valid_until": datetime.timestamp(datetime.now() + timedelta(minutes=5))}
             r = requests.post(self.proxy, headers={"Content-Type": "application/json"},
                               data=json.dumps(payload))
 
         payload = {"action": "add-header", "condition": self.condition_value,
-                   "value": self.proxy_condition}
+                   "value": self.proxy_condition, "valid_until": datetime.timestamp(datetime.now() + timedelta(minutes=5))}
         r = requests.post(self.proxy, headers={"Content-Type": "application/json"},
                           data=json.dumps(payload))
         self.logger.debug(r)
 
     def cleanup(self):
         self.logger.debug("IP Proxy cleanup")
-        payload = {"action": "block", "condition": self.conditionValue,
-                   "value": self.proxyCondition}
+        payload = {"action": "block", "condition": self.condition_value,
+                   "value": self.proxy_condition}
         r = requests.delete(self.proxy, headers={"Content-Type": "application/json"},
                             data=json.dumps(payload))
         payload = {"action": "add-header", "condition": self.condition_value,
@@ -94,15 +98,16 @@ class ActionSendMail(ActionDelegate):
 
         if os.path.isfile("last_mail"):
             with open("last_mail") as f:
-                now = datetime.datetime.now()
+                now = datetime.now()
                 content = f.read()
                 if content:
-                    dif = now - datetime.datetime.fromtimestamp(int(float(content)))
-                    if dif < datetime.timedelta(minutes=1):
+                    dif = now - \
+                        datetime.fromtimestamp(int(float(content)))
+                    if dif < timedelta(minutes=1):
                         return True
 
         with open("last_mail", "w") as f:
-            f.write(str(int(datetime.datetime.now().timestamp())))
+            f.write(str(int(datetime.now().timestamp())))
 
         self.logger.info("Sending Mail...")
 
