ed6a8b4d806a3b13fb6dea6c525f873f1a572d2d
Jaime Conde Segovia <conde-segovia.extern@univention.de>
2022-12-22 08:45:24 +0100

    enh(proxy): refactor

--- proxy/app.py
+++ proxy/app.py
@@ -1,6 +1,34 @@
-import main
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String
+import flask
 import os
 
 
-def create_app(envivorment=os.environ, start_response=None):
-    return main.app
+APP_NAME = "Smart Flask Reverse Proxy"
+
+app = flask.Flask(APP_NAME)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+
+
+db = SQLAlchemy(app)
+
+
+class File(db.Model):
+    __tablename__ = "devices"
+    uuid = Column(String, primary_key=True)
+    agent = Column(String)
+    ip = Column(String)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.before_first_request
+def init():
+    app.config["DB"] = db
+    app.config["API_SECRET"] = os.environ["API_SECRET"]
+    app.config["ACTIONS"] = {}
+    app.config["INDEX"] = {}
+    app.config["BACKEND"] = f"{os.environ['KEYCLOAK_PROTOCOL']}://{os.environ['KEYCLOAK_SERVER']}"
