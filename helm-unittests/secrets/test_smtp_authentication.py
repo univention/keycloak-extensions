be179e260822bf09adceb7c9884309d48f9e784b
Marius Meschter <marius.meschter@univention.de>
2025-11-13 14:46:45 +0100

    fix: do not require or template the secrets if smtp auth is disabled
    Issue univention/dev/internal/team-nubus#1525

--- helm-unittests/secrets/test_smtp_authentication.py
+++ helm-unittests/secrets/test_smtp_authentication.py
@@ -5,7 +5,8 @@ from univention.testing.helm.auth_flavors.password_usage import \
     AuthPasswordUsageViaEnv
 from univention.testing.helm.auth_flavors.secret_generation import \
     AuthSecretGenerationUser
-from univention.testing.helm.auth_flavors.username import AuthUsernameViaEnv
+from univention.testing.helm.auth_flavors.username import AuthUsernameViaEnv, pytest
+from univention.testing.helm.client.base import BaseTest
 
 
 class TestSmtpSecret(AuthSecretGenerationUser, AuthPasswordUsageViaEnv, AuthUsernameViaEnv):
@@ -16,3 +17,46 @@ class TestSmtpSecret(AuthSecretGenerationUser, AuthPasswordUsageViaEnv, AuthUser
     sub_path_env_password = "env[?@name=='SMTP_PASSWORD']"
     workload_name = "release-name-keycloak-extensions-handler"
     sub_path_env_username = "env[?@name=='SMTP_USERNAME']"
+
+
+class TestSmtpSecretNotTemplatedIfAuthDisabled(BaseTest):
+    def test_env_vars_not_templated(self, chart):
+        values = self.load_and_map(
+            """
+            smtp:
+              auth:
+                enabled: false
+                password: null
+                existingSecret:
+                  name: null
+            """)
+        result = chart.helm_template(values)
+
+        smtp_username = "env[?@name=='SMTP_USERNAME']"
+        smtp_password = "env[?@name=='SELF_PASSWORD']"
+
+        workload = result.get_resource(
+            kind="Deployment", name="release-name-keycloak-extensions-handler")
+        container = workload.findone(self.path_container)
+
+        with pytest.raises(LookupError):
+            container.findone(smtp_username)
+
+        with pytest.raises(LookupError):
+            container.findone(smtp_password)
+
+    def test_secret_not_templated(self, chart):
+        values = self.load_and_map(
+            """
+            smtp:
+              auth:
+                enabled: false
+                password: null
+                existingSecret:
+                  name: null
+            """)
+        result = chart.helm_template(values)
+
+        with pytest.raises(LookupError):
+            result.get_resource(
+                kind="Secret", name="release-name-keycloak-extensions-smtp-credentials")
